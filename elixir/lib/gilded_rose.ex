6b4afa8ab2d2a3bfc084c08feb336df9fbaaf9bd
Jacob Parry <jacobparry@Jacobs-MacBook-Pro.local>
2024-09-24 15:53:51 -0600

    refactored for Elixir

--- elixir/lib/gilded_rose.ex
+++ elixir/lib/gilded_rose.ex
@@ -7,77 +7,135 @@ defmodule GildedRose do
     Enum.map(items, &update_item/1)
   end
 
+  @doc """
+  When refactoring, I noticed that the code was going
+  1. Update Quality
+  2. Update Sell In
+  3. Update Quality Again
+
+  I decided to refactor the code to update the sell_in first, then update the quality.
+  I believe this only made a difference for the Backstage passes, and infact solves a quality bug for the Backstage passes.
+  """
   def update_item(item) do
-    item = cond do
-      item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" ->
-        if item.quality > 0 do
-          if item.name != "Sulfuras, Hand of Ragnaros" do
-            %{item | quality: item.quality - 1}
-          else
-            item
-          end
-        else
-          item
-        end
-      true ->
-        cond do
-          item.quality < 50 ->
-            item = %{item | quality: item.quality + 1}
-            cond do
-              item.name == "Backstage passes to a TAFKAL80ETC concert" ->
-                item = cond do
-                  item.sell_in < 11 ->
-                    cond do
-                      item.quality < 50 ->
-                        %{item | quality: item.quality + 1}
-                      true -> item
-                    end
-                  true -> item
-                end
-                cond do
-                  item.sell_in < 6 ->
-                    cond do
-                      item.quality < 50 ->
-                        %{item | quality: item.quality + 1}
-                      true -> item
-                    end
-                  true -> item
-                end
-              true -> item
-            end
-          true -> item
-        end
-    end
-    item = cond do
-      item.name != "Sulfuras, Hand of Ragnaros" ->
-        %{item | sell_in: item.sell_in - 1}
-      true -> item
-    end
-    cond do
-      item.sell_in < 0 ->
-        cond do
-          item.name != "Aged Brie" ->
-            cond do
-              item.name != "Backstage passes to a TAFKAL80ETC concert" ->
-                cond do
-                  item.quality > 0 ->
-                    cond do
-                      item.name != "Sulfuras, Hand of Ragnaros" ->
-                        %{item | quality: item.quality - 1}
-                      true -> item
-                    end
-                  true -> item
-                end
-              true -> %{item | quality: item.quality - item.quality}
-            end
-          true ->
-            cond do
-              item.quality < 50 ->
-                %{item | quality: item.quality + 1}
-              true -> item
-            end
-        end
-      true -> item
-    end
+    item
+    |> update_item_sell_in()
+    |> update_item_quality()
+    |> check_max_min_quality()
   end
+
+  @doc """
+    At the end of each day, it decreases sell_in 1 for all items.
+
+    However, Sulfuras, Hand of Ragnaros never has to be sold or decreases in quality.
+    I chose to interpret `never has to be sold` as `never decreases sell_in`, or rather, never changes the sell_in.
+  """
+  def update_item_sell_in(%Item{name: "Sulfuras, Hand of Ragnaros"} = item), do: item
+  def update_item_sell_in(%Item{} = item), do: %{item | sell_in: item.sell_in - 1}
+
+  @doc """
+  Sulfuras never changes in quality.
+
+  Aged Brie increases in quality as it ages
+
+  Backstage passes to a TAFKAL80ETC concert increases in quality as sell_in approaches
+  - quality increases by 1 when there are more than 10 days remaining
+  - quality increases by 2 when there are 6 to 10 days remaining
+  - quality increases by 3 when there are 1-5 days remaining
+  - quality drops to 0 after the concert
+
+  Normal items decrease in quality
+  - quality decreases by 1 when sell_in is greater than or equal to 0
+  - quality decreases by 2 when sell_in is less than 0
+
+  Conjured items decrease in quality twice as fast as normal items
+  - quality decreases by 2 when sell_in is greater than or equal to 0
+  - quality decreases by 4 when sell_in is less than 0
+
+  """
+  def update_item_quality(%Item{name: "Sulfuras, Hand of Ragnaros", quality: 80} = item), do: item
+
+  def update_item_quality(%Item{name: "Sulfuras, Hand of Ragnaros", quality: _} = item),
+    do: %{item | quality: 80}
+
+  def update_item_quality(
+        %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: sell_in} = item
+      )
+      when sell_in > 10 do
+    new_quality = item.quality + 1
+
+    %{item | quality: new_quality}
+  end
+
+  def update_item_quality(
+        %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: sell_in} = item
+      )
+      when sell_in > 5 and sell_in < 11 do
+    new_quality = item.quality + 2
+
+    %{item | quality: new_quality}
+  end
+
+  def update_item_quality(
+        %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: sell_in} = item
+      )
+      when sell_in >= 0 and sell_in < 5 do
+    new_quality = item.quality + 3
+
+    %{item | quality: new_quality}
+  end
+
+  def update_item_quality(
+        %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: sell_in} = item
+      )
+      when sell_in < 0 do
+    %{item | quality: 0}
+  end
+
+  def update_item_quality(%Item{name: "Aged Brie"} = item) do
+    new_quality = item.quality + 1
+
+    %{item | quality: new_quality}
+  end
+
+  def update_item_quality(%Item{name: "Conjured Mana Cake", sell_in: sell_in} = item)
+      when sell_in >= 0 do
+    new_quality = item.quality - 2
+
+    %{item | quality: new_quality}
+  end
+
+  def update_item_quality(%Item{name: "Conjured Mana Cake", sell_in: sell_in} = item)
+      when sell_in < 0 do
+    new_quality = item.quality - 4
+
+    %{item | quality: new_quality}
+  end
+
+  def update_item_quality(%Item{sell_in: sell_in} = item) when sell_in >= 0 do
+    new_quality = item.quality - 1
+
+    %{item | quality: new_quality}
+  end
+
+  def update_item_quality(%Item{sell_in: sell_in} = item) when sell_in < 0 do
+    new_quality = item.quality - 2
+    %{item | quality: new_quality}
+  end
+
+  @doc """
+    The quality of an item is never negative.
+    The quality of an item is never more than 50.
+    The quality of Sulfuras, Hand of Ragnaros is always 80
+  """
+  def check_max_min_quality(%Item{name: "Sulfuras, Hand of Ragnaros"} = item), do: item
+
+  def check_max_min_quality(%Item{quality: quality} = item) when quality >= 50 do
+    %{item | quality: 50}
+  end
+
+  def check_max_min_quality(%Item{quality: quality} = item) when quality <= 0 do
+    %{item | quality: 0}
+  end
+
+  def check_max_min_quality(item), do: item
 end
--- elixir/test/gilded_rose_test.exs
+++ elixir/test/gilded_rose_test.exs
@@ -1,10 +1,146 @@
 defmodule GildedRoseTest do
   use ExUnit.Case
 
-  test "begin the journey of refactoring" do
-    items = [%Item{name: "foo", sell_in: 0, quality: 0}]
-    GildedRose.update_quality(items)
-    %{name: firstItemName} = List.first(items)
-    assert "fixme" == firstItemName
+  describe "update_quality" do
+    test "begin the journey of refactoring" do
+      items = [%Item{name: "foo", sell_in: 0, quality: 0}]
+      GildedRose.update_quality(items)
+      first_item = List.first(items)
+      assert "foo" == first_item.name
+    end
+  end
+
+  describe "update_item/1" do
+    test "Normal items quality can be zero" do
+      item = %Item{name: "foo", sell_in: 0, quality: 1}
+      updated_item = GildedRose.update_item(item)
+      assert updated_item.quality == 0
+    end
+
+    test "Normal items quality can't be negative" do
+      item = %Item{name: "foo", sell_in: 0, quality: 0}
+      updated_item = GildedRose.update_item(item)
+      assert updated_item.quality == 0
+    end
+
+    test "Normal items sell_in can be negative" do
+      item = %Item{name: "foo", sell_in: 0, quality: 0}
+      updated_item = GildedRose.update_item(item)
+      assert updated_item.sell_in == -1
+    end
+
+    test "Normal items degrade by 1 for both sell_in and quality each day" do
+      item = %Item{name: "foo", sell_in: 1, quality: 1}
+      updated_item = GildedRose.update_item(item)
+      assert updated_item.quality == 0
+      assert updated_item.sell_in == 0
+    end
+
+    test "Normal items quality degrades twice as fast when sell_in is negative" do
+      item = %Item{name: "foo", sell_in: -1, quality: 4}
+      updated_item = GildedRose.update_item(item)
+      assert updated_item.quality == 2
+      assert updated_item.sell_in == -2
+
+      second_update = GildedRose.update_item(updated_item)
+      assert second_update.quality == 0
+      assert second_update.sell_in == -3
+    end
+
+    test "Normal items quality can't be more than 50" do
+      item = %Item{name: "foo", sell_in: 1, quality: 60}
+      updated_item = GildedRose.update_item(item)
+      assert updated_item.quality == 50
+    end
+
+    test "Sulfuras quality never changes" do
+      item = %Item{name: "Sulfuras, Hand of Ragnaros", sell_in: 1, quality: 80}
+      updated_item = GildedRose.update_item(item)
+      assert updated_item.quality == 80
+    end
+
+    test "Sulfuras sell_in never changes" do
+      item = %Item{name: "Sulfuras, Hand of Ragnaros", sell_in: 2, quality: 80}
+      updated_item = GildedRose.update_item(item)
+      assert updated_item.sell_in == 2
+    end
+
+    test "Aged Brie increases in quality as it ages" do
+      item = %Item{name: "Aged Brie", sell_in: 1, quality: 1}
+      updated_item = GildedRose.update_item(item)
+      assert updated_item.quality == 2
+      assert updated_item.sell_in == 0
+    end
+
+    test "Aged Brie quality can't be more than 50" do
+      item = %Item{name: "Aged Brie", sell_in: 1, quality: 49}
+      updated_item = GildedRose.update_item(item)
+      assert updated_item.quality == 50
+      assert updated_item.sell_in == 0
+
+      updated_item_again = GildedRose.update_item(updated_item)
+      assert updated_item_again.quality == 50
+      assert updated_item_again.sell_in == -1
+    end
+
+    test "Backstage passes increase in quality as sell_in approaches" do
+      item = %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 12, quality: 1}
+      updated_item = GildedRose.update_item(item)
+      assert updated_item.quality == 2
+      assert updated_item.sell_in == 11
+    end
+
+    test "Backstage passes increase in quality by 2 when there are 6 to 10 days remaining" do
+      # At the end of each day, it decreases sell_by 1. I am testing sell_bys for 6 to 10, so I start with 7-11
+      Enum.each(7..11, fn days_remaining ->
+        item = %Item{
+          name: "Backstage passes to a TAFKAL80ETC concert",
+          sell_in: days_remaining,
+          quality: 1
+        }
+
+        updated_item = GildedRose.update_item(item)
+        assert updated_item.quality == 3
+      end)
+    end
+
+    test "Backstage passes increase in quality by 3 when there are 1-5 days remaining" do
+      Enum.each(1..5, fn days_remaining ->
+        item = %Item{
+          name: "Backstage passes to a TAFKAL80ETC concert",
+          sell_in: days_remaining,
+          quality: 1
+        }
+
+        updated_item = GildedRose.update_item(item)
+        assert updated_item.quality == 4
+      end)
+    end
+
+    test "Backstage passes quality drops to 0 after the concert" do
+      item = %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 0, quality: 1}
+      updated_item = GildedRose.update_item(item)
+      assert updated_item.quality == 0
+    end
+
+    test "Backstages passes quality can't be more than 50" do
+      item = %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 1, quality: 49}
+      updated_item = GildedRose.update_item(item)
+      assert updated_item.quality == 50
+    end
+
+    test "Conjured items degrade in quality twice as fast as normal items when sell_in is not negative" do
+      item = %Item{name: "Conjured Mana Cake", sell_in: 3, quality: 6}
+      updated_item = GildedRose.update_item(item)
+      assert updated_item.quality == 4
+      assert updated_item.sell_in == 2
+    end
+
+    test "Conjured items degrade in quality twice as fast as normal items when sell_in is negative" do
+      item = %Item{name: "Conjured Mana Cake", sell_in: -1, quality: 6}
+      updated_item = GildedRose.update_item(item)
+      assert updated_item.quality == 2
+      assert updated_item.sell_in == -2
+    end
   end
 end
